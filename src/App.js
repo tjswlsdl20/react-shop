45deaa9718ba72ec0a1c3adcf2b27f52108ce21e
sjk <tjswlsdl20@gmail.com>
2021-05-07 15:59:49 +0900

    react-shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavDropdown, Form, Button, FormControl, Jumbotron } from 'react-bootstrap';
+import info from './data.js';
+import { useState } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import Detail from './Detail.js';
 
 function App() {
+
+
+  let [infos, setInfos] = useState(info);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand ><Link to="/">shoes-shoes</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+
+
+      <Route exact path='/'>
+        <Jumbotron className="background">
+          <h1>20%OFF</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+        <div className="container">
+          <div className="row">
+            <Shoe infos={infos} />
+
+          </div>
+        </div>
+      </Route>
+
+
+      <Route path='/detail'>
+
+        <Detail />
+      </Route>
     </div>
+
   );
 }
 
+function Shoe(props) {
+  return (
+    props.infos.map(
+      (shoeInfo, i) => {
+        return (
+
+          <div className="col-md-4">
+            <img src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"} width="100%" />
+            <h4>{shoeInfo.title}</h4>
+            <p>{shoeInfo.content}</p>
+            <p>{shoeInfo.price}</p>
+          </div>
+
+        )
+      }
+    )
+
+  )
+}
 export default App;
